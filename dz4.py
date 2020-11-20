6ba5d32d19db63201d3fa70b54e0c106b1fd55c0
subfor <alex.kudelko@gmail.com>
2020-11-20 18:29:13 +0200

    subfor

--- dz4.py
+++ dz4.py
@@ -22,15 +22,16 @@ try:
     print(value ** -1)
 except (ValueError, ZeroDivisionError):
     print("Это не число с запятой!")
+#############
 
 
 ##############
-my_string = '0123456789'
-my_tuple = tuple(my_string)
-my_list = []
-
-for index_tuple in my_tuple:
-    for index_tuple_1 in my_tuple:
-        num = int(index_tuple + index_tuple_1)
-        my_list.append(num)
-print(my_list)
+# my_string = '0123456789'
+# my_tuple = tuple(my_string)
+# my_list = []
+#
+# for index_tuple in my_tuple:
+#     for index_tuple_1 in my_tuple:
+#         num = int(index_tuple + index_tuple_1)
+#         my_list.append(num)
+# print(my_list)
