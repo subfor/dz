8869a5604b41922182a31105a8b7aaa5829f14b3
subfor <alex.kudelko@gmail.com>
2020-11-20 13:55:50 +0200

    subfor

--- test.py
+++ test.py
@@ -1,18 +1,10 @@
-# a = b = c = 3
-# a = 4
-# a = b
-# print(a)
-# print(b)
-# print(c)
 
 my_string = '0123456789'
 my_tuple = tuple(my_string)
-print(my_tuple)
-print(my_tuple[1], type(my_tuple[1]))
 my_list = []
-for index_tuple in my_tuple:
-    # print(index_tuple, type(index_tuple))
-    # print(index_tuple + index_tuple)
-    for index_tuple_1 in my_tuple:
-        print(index_tuple + index_tuple_1)
 
+for index_tuple in my_tuple:
+    for index_tuple_1 in my_tuple:
+        num = int(index_tuple + index_tuple_1)
+        my_list.append(num)
+print(my_list)
