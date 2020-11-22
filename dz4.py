8fc251f5775bfc7050dab49b776a1e4f9cf4d121
subfor <alex.kudelko@gmail.com>
2020-11-22 14:40:53 +0200

    subfor

--- dz4.py
+++ dz4.py
@@ -23,8 +23,16 @@ try:
 except (ValueError, ZeroDivisionError):
     print("Это не число с запятой!")
 #############
-
-
+my_list = [1, 55, 22, 107, 234, 0, 100, 23, 923, 2, 8, 105]
+my_indexes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+for  index in my_indexes:
+    print(my_list[index])
+#############
+my_list_1 = [1, 55, 22, 107, 234, 0, 100, 23, 923, 2, 8, 105]
+my_list_2 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"]
+my_indexes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+for index in my_indexes:
+    print("(", my_list_1[index], ",", my_list_2[index], ")")
 ##############
 my_string = '0123456789'
 my_tuple = tuple(my_string)
