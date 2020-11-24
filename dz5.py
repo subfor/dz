164724a48d46cc2d2cdd9d466ba73db163f16b48
subfor <alex.kudelko@gmail.com>
2020-11-24 11:31:10 +0200

    subfor

---
+++ dz5.py
@@ -0,0 +1,7 @@
+my_int = 2300400030
+# my_int_str = sorted(str(my_int))
+# my_int_str = int("".join(my_int_str))
+# my_int_str = len(str(my_int_str))
+my_int_str = str(my_int)
+count = my_int_str.count("0")
+print(count)
