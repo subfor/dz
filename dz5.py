e37b3e5818855434ed6c5d1c0fd96c5a3ccb8619
subfor <alex.kudelko@gmail.com>
2020-11-24 11:42:22 +0200

    subfor

--- dz5.py
+++ dz5.py
@@ -1,7 +1,11 @@
-my_int = 230040003000
-# my_int_str = sorted(str(my_int))
-# my_int_str = int("".join(my_int_str))
-# my_int_str = len(str(my_int_str))
+my_int = 23004000300
 my_int_str = str(my_int)
 count = my_int_str.count("0")
 print(count)
+
+#######################
+
+my_int = 23004000300
+print(len(str(my_int)) - len(str(int(str(my_int)[::-1]))))
+# my_int_str = int("".join(my_int_str))
+# my_int_str = len(str(my_int_str))
