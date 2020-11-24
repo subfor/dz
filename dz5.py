06807c3d958d176abd0b7b0f266e51eb1439378f
subfor <alex.kudelko@gmail.com>
2020-11-24 17:32:35 +0200

    subfor

--- dz5.py
+++ dz5.py
@@ -9,3 +9,34 @@ my_int = 23004000300
 print(len(str(my_int)) - len(str(int(str(my_int)[::-1]))))
 # my_int_str = int("".join(my_int_str))
 # my_int_str = len(str(my_int_str))
+######################
+
+my_list_1 = [1,2,3,4,5 ]
+my_list_2 = [10, 15, 20, 25]
+my_result = []
+# for index, value in enumerate(my_list_1):
+    # print(index, value)
+    # if index % 2:
+    #     my_result.append(value)
+    # print(my_list_1[index])
+for value_1 in my_list_1:
+    if not value_1 % 2:
+        my_result.append(value_1)
+for value_2 in my_list_2:
+    if value_2 % 2:
+        my_result.append(value_2)
+print(my_result)
+##################
+my_list = [1,2,3,4,5]
+new_list = []
+for index, value in enumerate(my_list):
+    if index > 0:
+        new_list.append(value)
+new_list.append(my_list[0])
+print(new_list)
+###################
+my_list = [1,2,3,4,5]
+last = my_list.pop()
+my_list.insert(0, last)
+print(my_list)
+####################
