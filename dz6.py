23ce0bf4a01dddc2616d308dbcb38c727cb587de
subfor <alex.kudelko@gmail.com>
2020-11-27 19:29:20 +0200

    subfor

--- dz6.py
+++ dz6.py
@@ -49,3 +49,14 @@ my_list_new = []
 my_list_new = list(set(my_str1).intersection(set(my_str2)))
 print(my_list_new, type(my_list_new))
 ###########################
+my_str1 = "aabccccdfrx xx r"
+my_str2 = "aa ccd f"
+my_list_new = []
+
+my_list_set = list(set(my_str1).intersection(set(my_str2)))
+my_unic = my_list_set.copy()
+my_list_new = [value for value in my_unic if my_str1.count(value) == 1 and my_str2.count(value) == 1]
+print(my_str1)
+print(my_str2)
+print(my_list_new)
+########################
