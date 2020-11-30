409631c64f6e85d0b154b9c0dd93adc37f20fe2e
subfor <alex.kudelko@gmail.com>
2020-11-30 11:25:14 +0200

    subfor

--- dz6.py
+++ dz6.py
@@ -9,6 +9,7 @@ my_list = ["aqwed", "23efadf", "dsf4fr", "adesce", "jdffa", "dfgr"]
 my_list_new = []
 
 my_list_new = [value for value in my_list if value[0] == "a"]
+
 print(my_list_new)
 ##########################
 
@@ -18,16 +19,17 @@ my_list_new = []
 my_list_new = [value for value in my_list if value.find("a") > -1]
 print(my_list_new)
 ##########################
-my_list = ["23", "aqwed", "23efadf", "76", "dsf4fr", "adesce", "jdffa", "7","dfgr"]
-my_list_new = []
+my_list = [23, "aqwed", "23efadf", 76, "dsf4fr", "adesce", "jdffa", 7, "dfgr"]
 
-my_list_new = [value for value in my_list if value.isdigit()]
+my_list_new = [value for value in my_list if not str(value).isdigit()]
+print(my_list)
 print(my_list_new)
 ##########################
-my_str = "aabccccdrx xx r"
+my_str = "aabccccdrx xx r v"
 my_list_new = []
 
 my_set_tmp = list(set(my_str))
+#тут пробовал копировать чтоб всегда один и тотже список выдавало
 my_set = my_set_tmp.copy()
 my_set.sort()
 my_list_new = [value for value in my_set if my_str.count(value) == 1]
@@ -39,14 +41,15 @@ my_str2 = "aa ccd f"
 my_list_new = []
 
 my_list_new = list(set(my_str1).intersection(set(my_str2)))
-print(my_list_new, type(my_list_new))
+print(my_str1)
+print(my_str2)
+print(my_list_new)
 ###########################
-my_str1 = "aabccccdfrx xx r"
-my_str2 = "aa ccd f"
+my_str1 = "aabccccdfrx xx z"
+my_str2 = "aa ccd f z"
 my_list_new = []
 
-my_list_set = list(set(my_str1).intersection(set(my_str2)))
-my_unic = my_list_set.copy()
+my_unic = set(my_str1).intersection(set(my_str2))
 my_list_new = [value for value in my_unic if my_str1.count(value) == 1 and my_str2.count(value) == 1]
 print(my_str1)
 print(my_str2)
--- test.py
+++ test.py
@@ -1,10 +1,9 @@
-numbers = [1, 3, 5, 6, 7, -2]
+my_str1 = "aabccccdfrx xx z"
+my_str2 = "aa ccd f z"
+my_list_new = []
 
-new_numbers = []
-# for number in numbers:
-#     if number > 0:
-#         new_numbers.append(number ** 2)
-
-new_numbers = [number ** 2 for number in numbers]
-
-print(new_numbers)
+my_unic = set(my_str1).intersection(set(my_str2))
+my_list_new = [value for value in my_unic if my_str1.count(value) == 1 and my_str2.count(value) == 1]
+print(my_str1)
+print(my_str2)
+print(my_list_new)
