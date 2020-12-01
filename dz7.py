a1fad9a748c0ae04534900173fb3d8f6bb692228
subfor <alex.kudelko@gmail.com>
2020-12-01 22:39:46 +0200

    subfor

--- dz7.py
+++ dz7.py
@@ -5,7 +5,7 @@ print(rand_list)
 
 ###################################
 
-triangle = {key: tuple(random.randint(-10, 10) for _ in range(2)) for key in "ABC"}
+triangle = {key: tuple(random.randint(-10, 10) for _ in range(2)) for key in "ABCDF"}
 print(triangle)
 print(triangle["A"], type(triangle["A"]))
 
@@ -37,4 +37,22 @@ for name in names_list:
 print("**************************")
 mid_age = sum(ages_list) // len(ages_list)
 print(mid_age, "лет")
+#print(persons[0].values())
+# print(persons[0].keys())
 ###################################
+my_dict_1 = {'A': 8, 'B': 7, 'C': 5, 'D': 3, 'F': 56}
+my_dict_2 = {'D': 23, 'L': 56, 'C': 23, 'Z': 7, 'Y': 1}
+
+my_set = list(set(my_dict_1.keys()).intersection(my_dict_2.keys()))
+print(my_dict_1.keys(), type(my_dict_1.keys()))
+print(len(my_dict_1))
+print(my_set)
+
+my_set2 = list(set(my_dict_1.keys()).difference(my_dict_2.keys()))
+print(my_set2)
+print(list(my_dict_1.items())[1])
+# my_new_dict = {my_dict_1.values() }
+my_new_dict = {key: my_dict_1[key] for key in my_set2}
+print(my_new_dict)
+# my_dict = {key: None for key in my_dict_1.key().}
+# print(my_dict)
