f1b4ddd4190789aa4982c60e1a3c8835ab45b901
subfor <alex.kudelko@gmail.com>
2020-12-02 23:09:11 +0200

    subfor

--- dz7.py
+++ dz7.py
@@ -23,7 +23,7 @@ my_print(my_str)
 persons = [{"name": "John", "age": 35}, {"name": "Bob", "age": 19}, {"name": "Jack", "age": 15},
            {"name": "Thomas", "age": 45}, {"name": "Richard", "age": 25}, {"name": "Richird", "age": 15}]
 
-ages_list= [value_age.get("age") for  value_age in persons]
+ages_list= [value_age.get("age") for value_age in persons]
 age_min = min(ages_list)
 for index_dict in persons:
     if index_dict.get("age") == age_min:
