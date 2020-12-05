f6e806afb8e4ff403e704b3464d57e8db9910278
subfor <alex.kudelko@gmail.com>
2020-12-05 13:56:58 +0200

    subfor

--- dz8.py
+++ dz8.py
@@ -1,15 +1,15 @@
-import os
+from os import path
 
 
 def read_domains(domains_file: str):
-    with open(os.path.join(path_dir, domains_file), 'r') as file:
+    with open(path.join(path_dir, domains_file), 'r') as file:
         lines = file.readlines()
     domain_list = [value_line.strip()[1:] for value_line in lines]
     return domain_list
 
 
 def read_names(names_file: str):
-    with open(os.path.join(path_dir, names_file), 'r') as file:
+    with open(path.join(path_dir, names_file), 'r') as file:
         lines = file.readlines()
     line = [list((value_line.strip().replace('\t', " ")).split(" ")) for value_line in lines]
     names_list = [index[1] for index in line]
@@ -19,4 +19,3 @@ def read_names(names_file: str):
 path_dir = "domains"
 print(read_domains("domains.txt"))
 print(read_names("names.txt"))
-
