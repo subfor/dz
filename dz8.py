6f0e778227ef84c926a722276fae713a0e4e904d
subfor <alex.kudelko@gmail.com>
2020-12-06 17:14:18 +0200

    subfor

--- dz8.py
+++ dz8.py
@@ -20,12 +20,11 @@ def read_names(names_file: str):
 
 def generate_email(count_emails=1):
     my_email_list = []
+    names_list = read_names('names.txt')
+    domains_list = read_domains('domains.txt')
     for _ in range(count_emails):
-        """Это для себя оставил, работает но с choice думаю лучше"""
-        # random_name = read_names('names.txt')[randint(0, len(read_names('names.txt'))-1)].lower()
-        # random_domain = read_domains('domains.txt')[randint(0, len(read_domains('domains.txt'))-1)]
-        random_name = str(choice(read_names('names.txt'))).lower()
-        random_domain = choice(read_domains('domains.txt'))
+        random_name = str(choice(names_list)).lower()
+        random_domain = choice(domains_list)
         random_domain_name = "".join(choice(ascii_lowercase) for _ in range(randint(5, 7)))
         my_email = f"{random_name}.{randint(100, 999)}@{random_domain_name}.{random_domain}"
         my_email_list.append(my_email)
