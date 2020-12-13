d5954610630539c537df8cdeca6c65b2415ad6da
subfor <alex.kudelko@gmail.com>
2020-12-13 20:15:16 +0200

    subfor

--- dz9.py
+++ dz9.py
@@ -5,25 +5,30 @@ from string import ascii_lowercase, digits
 from os import path
 
 
+def join_string(list_sentenses : list):
+    joined_list = [value for sublist in list_sentenses for value in sublist]
+    joined_string = f'{" ".join(joined_list)}\n'
+    return joined_string
+
+
 def generate_str():
     mystr = []
     result_string = ""
     random_len = randint(100, 1000)
-
     while len(result_string) <= random_len:
         random_sentense = []
+        """Generating sentetse 3 - 12 words"""
         for _ in range(randint(3, 12)):
-            # random_sentense = ["".join(choice(ascii_lowercase) for _ in range(randint(2, 12))) for _ in range(randint(3, 10))]
             random_word = ("".join(choice(ascii_lowercase) for _ in range(randint(2, 12))))
             random_digit = ("".join(choice(digits) for _ in range(randint(1, 5))))
+            """Random choice word or digit"""
             if randint(1, 10) // 2:
                 random_sentense.append(random_word)
             else:
                 random_sentense.append(random_digit)
         mystr.append(random_sentense)
-        # print(*[' '.join(m) for m in mystr], sep=' ')
-        flat_list = [item for sublist in mystr for item in sublist]
-        result_string = f'{" ".join(flat_list)}\n'
+        result_string = join_string(mystr)
+    """Set separators and capitalase"""
     for value in mystr:
         if not value[0].isdigit():
             value[0] = value[0].capitalize()
@@ -32,9 +37,9 @@ def generate_str():
                 s = randint(0, len(value) - 1)
                 value[s] = f"{value[s][:-1]}{choice(',:')}"
         value[-1] = f"{value[-1][:-1]}{choice('.!?')}"
-    flat_list = [item for sublist in mystr for item in sublist]
-    result_string = f'{" ".join(flat_list)}\n'
-    if len(result_string) > 998:
+    result_string = join_string(mystr)
+    """Cut off string if len > 1000"""
+    if len(result_string) > 1000:
         result_string = f"{result_string[:998 - len(result_string)]}{choice('.!?')}\n"
     return result_string
 
@@ -50,7 +55,7 @@ def generate_dict():
     return random_dict
 
 
-def generete_list():
+def generate_list():
     n = range(randint(3, 10))
     my_list = [[choice("01") for _ in n] for _ in range(randint(3, 10))]
     return my_list
@@ -60,12 +65,12 @@ def write_file(file_to_write: str):
     ext = file_to_write[file_to_write.rfind("."):]
     if ext == ".csv":
         with open(path.join(file_to_write), "w") as csv_file:
-            data = generete_list()
+            data = generate_list()
             writer = csv.writer(csv_file)
             writer.writerows(data)
     elif ext == ".json":
         with open(path.join(file_to_write), "w") as json_file:
-            json.dump(generate_dict(), json_file)
+            json.dump(generate_dict(), json_file, indent=1)
     elif ext == ".txt":
         with open(path.join(file_to_write), "w") as txt_file:
             txt_file.write(generate_str())
@@ -73,9 +78,7 @@ def write_file(file_to_write: str):
         print("Unsupported file format")
 
 
-# write_file("ewre1.jsonqq")
-# write_file("ewre1.csv")
-# write_file("ewre1.json")
-# write_file("ewre1.txt")
-a = generate_str()
-print(a, len(a))
+write_file("ewre1.jsonqq")
+write_file("ewre1.csv")
+write_file("ewre1.json")
+write_file("ewre1.txt")
