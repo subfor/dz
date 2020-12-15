71d0356c338c71941d340fefd52f15947afb3a1d
subfor <alex.kudelko@gmail.com>
2020-12-15 22:43:40 +0200

    subfor

--- dz10.py
+++ dz10.py
@@ -27,7 +27,7 @@ def sort_by_death_date(person: dict):
     return data_of_death
 
 
-
+print(read_json("data.json"))
 persons = sorted(read_json("data.json"), key=sort_by_text_len)
 print(persons)
 persons = sorted(read_json("data.json"), key=sort_by_name)
